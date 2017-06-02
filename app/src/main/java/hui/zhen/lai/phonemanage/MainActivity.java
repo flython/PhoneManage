112c3208d280b17ee5d0871ccccfeb80a7b7e640
DESKTOPFLY\Profe <vipxyz@vip.qq.com>
2017-06-02 14:36:25 +0800

    再次提交同步到笔记本

--- app/src/main/java/hui/zhen/lai/phonemanage/MainActivity.java
+++ app/src/main/java/hui/zhen/lai/phonemanage/MainActivity.java
@@ -1,5 +1,6 @@
 package hui.zhen.lai.phonemanage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -34,41 +35,44 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {     //注册浮动按钮，按钮的功能为添加新的联系人，并绑定监听器
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, add_contact.class);
+                startActivity(intent);
             }
         });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate thenu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.menu_settings) {
-            Toast.makeText(this, "set", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        else if(id == R.id.menu_add) {
-            Toast.makeText(this, "add", Toast.LENGTH_SHORT).show();
-            return true;
-        }
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.menu_settings) {
+                Toast.makeText(this, "set", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            else if(id == R.id.menu_add) {
+                Toast.makeText(this, "add", Toast.LENGTH_SHORT).show();
+                return true;
+            }
 
-        return super.onOptionsItemSelected(item);
-    }
+            return super.onOptionsItemSelected(item);
+        }
 
     private void intiCon() {
         for (int i = 0; i <20 ; i++) {
